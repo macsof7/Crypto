3948202eadef400e7dca93c425acfb901bbe0d2e
Sofia Machalkova <sofi.machalkova@gmail.com>
2025-12-12 11:58:18 +0100

    Implementace portfolia kryptoměn - Model, Service, Controller a základní konfigurace

--- src/main/java/com/example/Crypto/CryptoApplication.java
+++ src/main/java/com/example/Crypto/CryptoApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CryptoApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoApplication.class, args);
 	}
-
+	
 }
